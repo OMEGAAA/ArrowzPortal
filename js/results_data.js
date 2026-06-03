aec5064363e2d6805bfee230c897c230556f71c6
OMEGAAA <usahama0430@ezweb.ne.jp>
2026-06-03 20:42:52 +0900

    feat: add news data for May and March field tests

--- js/results_data.js
+++ js/results_data.js
@@ -1,5 +1,5 @@
 window.RESULTS_DATA = {
-    "comment": "今月は全体的にジャンプ力が向上しました！",
+    "comment": "今月は自己ベスト更新者が多く、日々のトレーニングの成果が見られる結果となりました。",
     "loginLink": "https://drive.google.com/drive/folders/1eiqbKnCbcv80iE7pFOWnwh1apFcUn_vg?usp=sharing",
     "summaryImg": "images/ft_summary_img.png",
     "imgCaption": "フィールドテスト結果まとめ（2026.01.09更新）",
