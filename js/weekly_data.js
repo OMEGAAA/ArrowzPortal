66e7f774dcb335f8fea1a8dc773cd8475bdaab1b
OMEGAAA <usahama0430@ezweb.ne.jp>
2026-08-17 09:39:42 +0900

    feat: create weekly_data.js for handling weekly portal data

--- js/weekly_data.js
+++ js/weekly_data.js
@@ -5,7 +5,7 @@ window.WEEKLY_DATA = {
         "desc": "【MONTHLY SCHEDULE】<br>Week 1-3: Skill & Physical Training<br>Week 4: Field Test (FT)",
         "link": "menu.html",
         "btnText": "CHECK",
-        "pageDesc": "7月第1週目のトレーニングメニュー",
+        "pageDesc": "8月のトレーニングメニュー",
         "contents": {
             "agility": "-",
             "power": "-",
@@ -16,11 +16,11 @@ window.WEEKLY_DATA = {
     "theme": {
         "category": "THEME",
         "title": "MONTHLY THEME",
-        "desc": "7月の強化テーマ: <strong>跳ぶ力を伸ばす</strong><br>ジャンプする姿勢、高く遠くに",
+        "desc": "8月の強化テーマ: <strong>跳ぶ力を伸ばす</strong><br>ジャンプする姿勢、高く遠くに",
         "link": "theme.html",
         "btnText": "CHECK",
         "pageTitle": "JUMP",
-        "lead": "<strong>「爆発的加速」</strong><br>7月のテーマは、ジャンプ力を伸ばすためのトレーニングです。",
+        "lead": "<strong>「爆発的加速」</strong><br>8月のテーマは、ジャンプ力を伸ばすためのトレーニングです。",
         "body": "上・前に跳ぶ力を身に着けるために大切なポイントを確認しましょう。",
         "points": [
             "ヒップヒンジの姿勢",
@@ -31,7 +31,7 @@ window.WEEKLY_DATA = {
     "schedule": {
         "category": "SCHEDULE",
         "title": "CLOSED DAYS",
-        "desc": "【7月の休館日】<br>・7月18日 (土),7月19日（月）<br>※ほかの曜日に振替は可能です",
+        "desc": "【8月の休館日】<br>※ほかの曜日に振替は可能です",
         "link": "schedule.html",
         "btnText": "CHECK"
     }
